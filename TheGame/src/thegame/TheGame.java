6a5d409123b344837bb8fa17a9127e52fce6fa89
Corvus 1640 <35952736+BuhMo@users.noreply.github.com>
2018-10-24 01:54:32 -0700

    Update TheGame.java
    Tercera modificación

--- TheGame/src/thegame/TheGame.java
+++ TheGame/src/thegame/TheGame.java
@@ -5,6 +5,7 @@ public class TheGame {
     public static void intro() {
         System.out.print("¿Donde estoy?..."
                 + "¿qué ago aquí?...");
-        System.out.print("¿Que es este lugar?...");
+        System.out.print("¿Que es este lugar?..."
+                        +"¿Qué es este dolor de cabeza? *gemidos de dolor*");
     }
 }
